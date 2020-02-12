e6f61ac120220d31fc0deadd44d06af6ae0810fb
Mahlarian <rocharger14@gmail.com>
2020-02-12 12:57:13 -0500

    Standard push, no changes made

---
+++ commands/kick.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'kick',
+    usage: 'kick <@user> (reason)',
+    description: 'Kicks the user, with optional reason',
+    guildOnly: true,
+    requiredPermissions: [ 'KICK_MEMBERS' ],
+    botPermissions: [ 'KICK_MEMBERS' ],
+	execute(message, client, _) {
+
+	},
+};
